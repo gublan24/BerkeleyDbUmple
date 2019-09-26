202fcf50cd34c2bb4f19280af6a7aa5090fa2f8a
Abdulaziz <gublan24@gmail.com>
2019-09-26 15:54:34 -0400

    adding umple files and java files.

---
+++ javaFiles/LoggingConsoleHandler_umplified/com/sleepycat/je/util/DbRunAction.java
@@ -0,0 +1,12 @@
+// Original file location:  /home/abdulaziz/Desktop/BerkeleyDb/javaFiles/LoggingConsoleHandler_x/com/sleepycat/je/util/DbRunAction.java
+package com.sleepycat.je.util;
+public class DbRunAction {
+// START_OF_INNER_ELEMENT 
+// @MethodObject static class DbRunAction_main {
+//     protected void hook848() throws Exception {
+//       envConfig.setConfigParam(EnvironmentParams.JE_LOGGING_CONSOLE.getName(),"true");
+//       original();
+//     }
+//   }
+// END_OF_INNER_ELEMENT 
+}
